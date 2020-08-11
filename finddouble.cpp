41ee80fd528e94296f37a4b01958ff57dee700b3
Amrik Singh Khalsa <singhamrikkhalsa@gmail.com>
2020-08-12 00:57:34 +0530

    hello lets begin!

---
+++ finddouble.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL)
+
+#define endl "\n"
+
+using namespace std;
+
+int main()
+{
+    fastio;
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+   // code goes here
+        int t;
+        cin >> t;
+        int i,j,n;
+        cin>>n;
+        int arr[n];
+        for (int i = 0; i <n; i++){
+            cin>>arr[i];
+        }
+        for()
+        
+    }
+    
+    return 0;
+}
+
