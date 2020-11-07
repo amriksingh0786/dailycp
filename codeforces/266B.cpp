6114e1513597966be12cdf354fc82b2a82d1ff91
Amrik Singh Khalsa <singhamrikkhalsa@gmail.com>
2020-11-07 18:41:17 +0530

    266B submitted

---
+++ codeforces/266B.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL)
+
+#define endl "\n"
+
+using namespace std;
+
+int main()
+{
+    fastio;
+    int n, t;
+    string s;
+    cin >> n >> t >> s;
+    while (t--)
+    {
+        for (int i = 1; i < n; ++i)
+        {
+            if (s[i] == 'G' && s[i-1] == 'B')
+            {
+                s[i] = 'B';
+                s[i-1] = 'G';
+                ++i;
+            }
+        }
+    }
+    cout << s << endl;
+    return 0;
+}
